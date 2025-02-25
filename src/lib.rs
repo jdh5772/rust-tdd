37a7aad0d8c18048ff2f682cd4ddd458616b7492
jdh5772 <moonstne@naver.com>
2025-02-25 09:55:56 +0900

    Franc 추가 후 Money 트레이트로 중복 제거

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,38 @@
-pub struct Dollar {
-    pub amount: usize,
+pub trait Money: Sized {
+    fn new(amount: usize) -> Self;
+    fn amount(&self) -> usize;
+    fn multiple(&self, n: usize) -> Self {
+        Self::new(self.amount() * n)
+    }
+    fn equals(&self, other: Self) -> bool {
+        self.amount() == other.amount()
+    }
 }
 
-impl Dollar {
-    pub fn new(amount: usize) -> Self {
+#[derive(PartialEq, Eq, Debug)]
+pub struct Dollar {
+    amount: usize,
+}
+
+impl Money for Dollar {
+    fn new(amount: usize) -> Self {
         Self { amount }
     }
-    pub fn multiple(&self, n: usize) -> Self {
-        Self {
-            amount: self.amount * n,
-        }
+    fn amount(&self) -> usize {
+        self.amount
     }
-    pub fn equals(&self, dollar: Dollar) -> bool {
-        self.amount == dollar.amount
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct Franc {
+    amount: usize,
+}
+
+impl Money for Franc {
+    fn new(amount: usize) -> Self {
+        Self { amount }
+    }
+    fn amount(&self) -> usize {
+        self.amount
     }
 }
--- tests/dollar.rs
+++ tests/dollar.rs
@@ -1,13 +1,14 @@
-use rust_tdd::Dollar;
+use rust_tdd::{Dollar, Franc, Money};
 
 #[test]
 fn multiple_test() {
     let dollar_5 = Dollar::new(5);
-    assert_eq!(5, dollar_5.amount);
-    let dollar_10 = dollar_5.multiple(2);
-    assert_eq!(10, dollar_10.amount);
-    let dollar_15 = dollar_5.multiple(3);
-    assert_eq!(15, dollar_15.amount);
+    assert_eq!(Dollar::new(10), dollar_5.multiple(2));
+    assert_eq!(Dollar::new(15), dollar_5.multiple(3));
+
+    let franc_5 = Franc::new(5);
+    assert_eq!(Franc::new(10), franc_5.multiple(2));
+    assert_eq!(Franc::new(15), franc_5.multiple(3));
 }
 
 #[test]
@@ -15,4 +16,8 @@ fn equal_test() {
     let dollar_5 = Dollar::new(5);
     assert!(dollar_5.equals(Dollar::new(5)));
     assert!(!dollar_5.equals(Dollar::new(7)));
+
+    let franc_5 = Franc::new(5);
+    assert!(franc_5.equals(Franc::new(5)));
+    assert!(!franc_5.equals(Franc::new(7)));
 }
